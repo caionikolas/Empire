e840aacdc6768cf64cf36186803d73b7a45f6eed
caionikolas <Caio.kanu@hotmail.com>
2023-09-06 15:59:59 -0300

    wrap

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,45 +1,55 @@
 import Image from "next/image"
-import Logo from "../../assets/images/logo.svg"
+import Logo from "../../assets/images/logo-color.svg"
 import "./footer.css"
 import { lilitaOne } from "@/app/layout"
-import Ifood from '../../assets/images/ifood.svg'
-import Instagram from '../../assets/images/instagram.svg'
-import Barra from '../../assets/images/barra.svg'
-import Wpp from '../../assets/images/whatsapp.svg'
+import Ifood from '../../assets/images/ifood-color.svg'
+import Instagram from '../../assets/images/instagram-color.svg'
 
 export default function Footer() {
     return (
         <>
             <div id="footer">
-                <a href="#start">
-                    <div className="logo">
-                        <div className="img" >
-                            <div className={lilitaOne.className}>
-                                <Image 
-                                    src={Logo}
-                                    alt='logo Empire Burger'
-                                />
+                <div className="end">
+                    <a href="#start">
+                        <div className="logo">
+                            <div className="img" >
+                                <div className={lilitaOne.className}>
+                                    <Image 
+                                        src={Logo}
+                                        alt='logo Empire Burger'
+                                    />
+                                </div>
                             </div>
+                            <h1>EMPIRE <span>BURGER</span></h1>
                         </div>
-                        <h1>EMPIRE <span>BURGER</span></h1>
+                    </a>
+                    <div className="menu-nav-web">
+                        <a href="#start" className="active">Home</a>
+                        <a href="#menu">Localização</a>
+                        <a href="#menu">Cardápio</a>
+                        <a href="#localization">Sobre</a>
                     </div>
-                </a>
-                <div className="menu-nav">
-                    <a href="#start" className="active">Home</a>
-                    <a href="#menu">Localização</a>
-                    <a href="#menu">Cardápio</a>
-                    <a href="#localization">Sobre</a>
+                    <div className="menu-icon">
+                        <Image 
+                            src={Ifood}
+                            alt='icone do Ifood'
+                            className='ifood'
+                        />
+                        <Image 
+                            src={Instagram}
+                            alt='icone do Instagram'
+                            className='instagram'
+                        />
+                    </div>
+                    <div className="menu-nav">
+                        <a href="#start" className="active">Home</a>
+                        <a href="#menu">Localização</a>
+                        <a href="#menu">Cardápio</a>
+                        <a href="#localization">Sobre</a>
+                    </div>
                 </div>
-                <div className="menu-icon">
-                    <Image 
-                        src={Ifood}
-                        alt='icone do Ifood'
-                        className='ifood'
-                    />
-                    <Image 
-                        src={Instagram}
-                        alt='icone do Instagram'
-                    />
+                <div className="copy">
+                    <p>2022 © EmpireBurger. <span>Todos os direitos reservados.</span></p>
                 </div>
             </div>
         </>
